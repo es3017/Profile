bf768744d27eb1ec877e2afe8400a3a7a9976372
es3017 <78827803+es3017@users.noreply.github.com>
2023-10-22 20:30:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#name").text("Ethan Smith");
+    $("#major").text("CGT");
+    $("#year").text("Senior");
+
+    $("h4").html("<em><strong>INDIANA</strong></em>");
+
+    function customFunction() {
+        console.log("This is a custom function.");
+    }
+
+    customFunction();
+});
